afa2e976f9d60bcd584e8e665644d96e543eba11
cumironin <onepoint07@gmail.com>
2023-10-31 08:09:08 +0700

    update user

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -25,6 +26,8 @@ app.get("/test", (req, res) => {
     message: "Hello World",
   });
 });
+
+app.use(cookieParser());
 
 app.use(express.json());
 app.use("/api/user", userRouter);

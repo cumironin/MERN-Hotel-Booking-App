c96b8f738cd58a661e74de0c6df5eafe8dd78bda
cumironin <onepoint07@gmail.com>
2023-11-04 13:58:53 +0700

    create get user listings

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,14 +3,16 @@ import {
   testController,
   updateUser,
   deleteUser,
+  getUserListings,
 } from "../controllers/user.controller.js ";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/test", testController);
+// router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
-// router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
+// router.get("/:id", verifyToken, getUser);
 
 export default router;
